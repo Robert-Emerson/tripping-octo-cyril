7992fc852eea47c0b7bd15c75191ab1e95d07bbc
Robert Emerson <roe2pj@virginia.edu>
2014-02-16 13:14:53 -0500

    server.py now handles url params correctly

--- server.js
+++ server.js
@@ -12,42 +12,43 @@ var connection = mysql.createConnection({
 var server = http.createServer(function (request, response) {
     var pathname = url.parse(request.url).pathname;
     var url_parts = url.parse(request.url, true);
-    var query = url_parts.query;
+    var query = url_parts.pathname;
     response.writeHead(200, {"Content-Type": "text/html"});
-    var lensVar = query["lens"];
-    if (typeof lensVar == "undefined") {
-       lensVar = "";
+
+    if (query.charAt(query.length-1) == "/") {
+        query = query.slice(0,-1);
     }
-    switch(lensVar.toLowerCase())
+
+    switch(query.toLowerCase())
     {
-    case "pentax":
+    case "/pentax":
             connection.query('SELECT id, body, lens FROM picture_data WHERE lens = \'Pentax 55mm\';', function (error, rows, fields){
         response.write("<h1>Displays the image ID, body used, and lens used for each photo from my flickr feed </h1>");
-        response.write("<h2>To filter this by lens type add /?lens=</h2>");
+        response.write("<h2>To filter this by lens type add /<i>lenstype</i> to the URL</h2>");
         response.write("<h3>Lens types are Canon, Vivitar, Pentax or Unknown</h3>")
         response.write(JSON.stringify(rows));
         response.end();
         });
-    case "vivitar":
+    case "/vivitar":
         connection.query('SELECT id, body, lens FROM picture_data WHERE lens = \'Vivitar 70-210mm\';', function (error, rows, fields){
             response.write("<h1>Displays the image ID, body used, and lens used for each photo from my flickr feed </h1>");
-            response.write("<h2>To filter this by lens type add /?lens=</h2>");
+            response.write("<h2>To filter this by lens type add /<i>lenstype</i> to the URL</h2>");
             response.write("<h3>Lens types are Canon, Vivitar, Pentax or Unknown</h3>")
             response.write(JSON.stringify(rows));
             response.end();
         });
-    case "canon":
+    case "/canon":
         connection.query('SELECT id, body, lens FROM picture_data WHERE lens = \'Canon 18-55mm\';', function (error, rows, fields){
             response.write("<h1>Displays the image ID, body used, and lens used for each photo from my flickr feed </h1>");
-            response.write("<h2>To filter this by lens type add /?lens=</h2>");
+            response.write("<h2>To filter this by lens type add /<i>lenstype</i> to the URL</h2>");
             response.write("<h3>Lens types are Canon, Vivitar, Pentax or Unknown</h3>")
             response.write(JSON.stringify(rows));
             response.end();
     });
-    case "unknown":
+    case "/unknown":
         connection.query('SELECT id, body, lens FROM picture_data WHERE lens = \'Unknown\';', function (error, rows, fields){
             response.write("<h1>Displays the image ID, body used, and lens used for each photo from my flickr feed </h1>");
-            response.write("<h2>To filter this by lens type add /?lens=</h2>");
+            response.write("<h2>To filter this by lens type add /<i>lenstype</i> to the URL</h2>");
             response.write("<h3>Lens types are Canon, Vivitar, Pentax or Unknown</h3>")
             response.write(JSON.stringify(rows));
             response.end();
@@ -55,7 +56,7 @@ var server = http.createServer(function (request, response) {
     default:
         connection.query('SELECT id, body, lens FROM picture_data;', function (error, rows, fields){
             response.write("<h1>Displays the image ID, body used, and lens used for each photo from my flickr feed </h1>");
-            response.write("<h2>To filter this by lens type add /?lens=<i>something</i> to the URL</h2>");
+            response.write("<h2>To filter this by lens type add /<i>lenstype</i> to the URL</h2>");
             response.write("<h3>Lens types are Canon, Vivitar, Pentax or Unknown</h3>")
             response.write(JSON.stringify(rows));
             response.end();
